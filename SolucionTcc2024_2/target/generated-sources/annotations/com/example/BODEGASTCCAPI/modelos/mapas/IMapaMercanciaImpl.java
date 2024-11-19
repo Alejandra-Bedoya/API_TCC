04c5aaf24ee393e2e7546a3615c3f2ffdedc6d51
mabedoya <cesde.alejandra@gmail.com>
2024-11-19 10:36:32 -0500

    Se realizan pruebas en el front

--- SolucionTcc2024_2/target/generated-sources/annotations/com/example/BODEGASTCCAPI/modelos/mapas/IMapaMercanciaImpl.java
+++ SolucionTcc2024_2/target/generated-sources/annotations/com/example/BODEGASTCCAPI/modelos/mapas/IMapaMercanciaImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-11-19T06:22:36-0500",
+    date = "2024-11-19T10:02:48-0500",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 22.0.1 (Oracle Corporation)"
 )
 @Component
